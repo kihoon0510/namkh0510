20a0a38091f1d7fdb7aea616ff0153f15f9fd8b0
namkh0510 <nam_0510@naver.com>
2021-08-03 14:59:45 +0900

    중간 정리- 리드미 수정 및 패키지 수정

---
+++ eclipse-workspace/p01001/src/intern/no04/Stack.java
@@ -0,0 +1,64 @@
+package intern.no04;
+
+public class Stack {
+	
+	private static int nowPoint=1;
+	private Object[] array;
+	
+	
+	public Stack(int size) {
+		array = new Object[size];
+	}
+	
+	public void push(Object object) {
+		
+		array[nowPoint] = object;
+		nowPoint++;
+		show();
+	}
+	
+	public void pop() {
+		if(nowPoint>1)nowPoint--;
+		array[nowPoint] = null;
+		show();
+	}
+	
+	public Object top() {
+		System.out.println("Top: "+array[nowPoint-1]);
+		return array[nowPoint-1];
+	}
+	
+	public void isEmpty() {
+		if(nowPoint ==0) {
+			System.out.println("isEmpty: False");
+		}
+		else if(array[nowPoint-1] !=null) {
+			System.out.println("isEmpty: False");
+		}else {
+		System.out.println("isEmpt: True");
+		}
+	}
+	
+	public void isFull() {
+		
+		if(nowPoint ==0) {
+			System.out.println("isFull: True");
+		}
+		else if(array[nowPoint-1] !=null) {
+			System.out.println("isFull: True");
+		}else {
+		System.out.println("isFull: False");
+		}
+	}
+	
+	
+	
+	private void show() {
+		System.out.print("[");
+		for(int i=1; i<nowPoint; i++) {
+			System.out.print(array[i]+", ");
+		}
+		System.out.println("]");
+		System.out.println("현재 위치: "+nowPoint);
+	}
+}
