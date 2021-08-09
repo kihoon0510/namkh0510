e1b620ed070e1ed57912b30c6e27cd6a7023ad44
namkh0510 <nam_0510@naver.com>
2021-08-09 12:48:59 +0900

    no18 줄바꿈문제 수정

--- eclipse-workspace/p01001/src/MainClass.java
+++ eclipse-workspace/p01001/src/MainClass.java
@@ -193,10 +193,27 @@ public class MainClass {
 //		System.out.println(v);
 //		System.out.println(s);
 		
-		String title = "\"last_nam,e\", \"goo,d\"\"firs,,t_name\"\"\", \"ema,il\", \"gender\", \"birthday\", \"profession\", \"location\", \"status\", \"interests\", \"seeking\"";
-
+		String title = "\"last_name\", \"first_name\", \"email\", \"gender\", \"birthday\", \"profession\", \"location\", \"status\", \"interests\", \"seeking\"\r\n";
+		String content = "\"Anderson\", \"Jillian\", \"jill_anderson@breakneckpizza.com\", \"F\", \"1980-09-05\", \"TechnicalWriter\", \"PaloAlto,CA\", \"single\", \"kayaking,reptiles\", \"relationship,friends\"\r\n"
+				+ "\"Kenton\", \"Leo\", \"lkenton@starbuzzcoffee.com\", \"M\", \"1974-01-10\", \"Manager\", \"SanFrancisco,CA\",\"divorced\", \"women\", \"womentodate\"\r\n"
+				+ "\"McGavin\", \"Darrin\", \"captainlove@headfirsttheater.com\", \"M\", \"1966-01-23\", \"CruiseShipCaptain\",\"SanDiego,CA\", \"single\", \"sailing,fishing,yachting\", \"womenforcasualrelationships\"\r\n"
+				+ "\"Franklin\", \"Joe\", \"joe_franklin@leapinlimos.com\", \"M\", \"1977-04-28\", \"SoftwareSales\", \"Dallas,TX\", \"married\", \"fishing,drinking\", \"newjob\"\r\n"
+				+ "\"Hamilton\", \"Jamie\", \"dontbother@starbuzzcoffee.com\", \"F\", \"1964-09-10\", \"SystemAdministrator\", \"Princeton,NJ\", \"married\", \"RPG\", \"nothing\"\r\n"
+				+ "\"Chevrolet\", \"Maurice\", \"bookman4u@objectville.net\", \"M\", \"1962-07-01\", \"BookshopOwner\", \"MountainView,CA\", \"married\", \"collectingbooks,scubadiving\", \"friends\"\r\n"
+				+ "\"Kroger\", \"Renee\", \"poorrenee@mightygumball.net\", \"F\", \"1976-12-03\", \"Unemployed\", \"SanFrancisco,CA\", \"divorced\", \"cooking\", \"employment\"\r\n"
+				+ "\"Mendoza\", \"Angelina\", \"angelina@starbuzzcoffee.com\", \"F\", \"1979-08-19\", \"UNIXSysadmin\", \"SanFrancisco,CA\", \"married\", \"acting,dancing\", \"newjob\"\r\n"
+				+ "\"Murphy\", \"Donald\", \"padraic@tikibeanlounge.com\", \"M\", \"1967-01-23\", \"ComputerProgrammer\", \"NewYorkCity,NY\", \"committedrelationsh\", \"RPG,anime\", \"friends\"\r\n"
+				+ "\"Spatner\", \"John\", \"jpoet@objectville.net\", \"M\", \"1963-04-18\", \"Salesman\", \"Woodstock,NY\", \"married\", \"poetry,screenwriting\", \"nothing\"\r\n"
+				+ "";
+		
+		ArrayList<String> test = new ArrayList<String>();
 		System.out.println(title);
 		ArrayToTable arrayToTable = new ArrayToTable();
 		arrayToTable.title(title);
+		test = arrayToTable.contents(content);
+		System.out.println(test.get(9));
+		System.out.println(test.get(10));
+		
+		
 	}
 }
--- eclipse-workspace/p01001/src/intern/no18/ArrayToTable.java
+++ eclipse-workspace/p01001/src/intern/no18/ArrayToTable.java
@@ -4,72 +4,77 @@ import java.util.ArrayList;
 
 public class ArrayToTable {
 
-	ArrayList<String> titleList = new ArrayList<String>();
-	ArrayList<String> contentList = new ArrayList<String>();
-	String title;
-	String contents;
+	private ArrayList<String> titleList = new ArrayList<String>();
+	private ArrayList<String> contentList = new ArrayList<String>();
+
 	char[] chars;
+	int titleCount = 0, contentCount = 0;
 
-	public void title(String str) {
-		this.title = str;
-		convertTitleToArray(title);
-		System.out.println(titleList);
-
+	public ArrayList<String> title(String str) {
+		convertToArray(str, titleList);
+		titleCount = titleList.size();
+		System.out.println(titleList + "titleCount: " + titleCount);
+		
+		return titleList;
 	}
 
-	public void contents(String str) {
-		this.contents = str;
+	public ArrayList<String> contents(String str) {
+		convertToArray(str, contentList);
+		contentCount = contentList.size();
+		System.out.println(contentList + "contentCount: " + contentCount);
 
+		return contentList;
 	}
 
-	public void convertTitleToArray(String s) {
+	private void convertToArray(String s, ArrayList<String> list) {
 		StringBuffer buffer;
-		int commaStartPoint = 0; // ,시작 위치.
+		int dataStartPoint = 0; // ,시작 위치.
 		chars = s.toCharArray();
 		int doubleQuotesEndPoint = 0, doubleQuotesStartPoint = 0; // "의 마지막 위치.
 		for (int i = 0; i < chars.length; i++) {
 			if (chars[i] == '"') {
 				doubleQuotesStartPoint = i;
-				doubleQuotesEndPoint = findNextDoubleQuotes(i);
+				doubleQuotesEndPoint = findNextDoubleQuotes(i, s);
 				i = doubleQuotesEndPoint;
 			} else if (chars[i] == ',') {
 				buffer = new StringBuffer();
-				buffer.append(s.substring(commaStartPoint, doubleQuotesStartPoint));
-				buffer.append(s.substring(doubleQuotesStartPoint+1, doubleQuotesEndPoint).replace("\"\"", "\""));
-				buffer.append(s.substring(doubleQuotesEndPoint+1, i));
-				titleList.add(buffer.toString());
-				commaStartPoint = i + 1;
+				buffer.append(s.substring(dataStartPoint, doubleQuotesStartPoint));
+				buffer.append(s.substring(doubleQuotesStartPoint + 1, doubleQuotesEndPoint).replace("\"\"", "\""));
+				buffer.append(s.substring(doubleQuotesEndPoint + 1, i));
+				list.add(buffer.toString());
+				dataStartPoint = i + 1;
 			}
-			if(chars[i]=='\n' || i== chars.length-1) {
+
+			if (chars[i] == '\n' || i == chars.length - 1) {
 				buffer = new StringBuffer();
-				buffer.append(s.substring(commaStartPoint, doubleQuotesStartPoint));
-				buffer.append(s.substring(doubleQuotesStartPoint+1, doubleQuotesEndPoint).replace("\"\"", "\""));
-				buffer.append(s.substring(doubleQuotesEndPoint, i));
-				titleList.add(buffer.toString());
-				commaStartPoint = i + 1;
+				buffer.append(s.substring(dataStartPoint, doubleQuotesStartPoint));
+				buffer.append(s.substring(doubleQuotesStartPoint + 1, doubleQuotesEndPoint).replace("\"\"", "\""));
+				buffer.append(s.substring(doubleQuotesEndPoint+1, i));
+				buffer.deleteCharAt(buffer.length()-1);
+				list.add(buffer.toString());
+				dataStartPoint = i + 1;
 			}
-			
 		}
 	}
 
-	private int findNextDoubleQuotes(int startPoint) {
+	private int findNextDoubleQuotes(int startPoint, String str) {
 		int endPoint = 0;
-		for (int i = startPoint + 1; i < title.length(); i++) {
-			
-				try {
-					if (chars[i] == '"') {
-						if (chars[i + 1] == '"') {
-							i++;
-							continue;
-						}
-						endPoint = i;
-						return endPoint;
+		for (int i = startPoint + 1; i < str.length(); i++) {
+
+			try {
+				if (chars[i] == '"') {
+					if (chars[i + 1] == '"') {
+						i++;
+						continue;
 					}
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					return title.length()-1;
+					endPoint = i;
+					return endPoint;
 				}
-			
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				return str.length() - 1;
+			}
+
 		}
 		return 0;
 	}
